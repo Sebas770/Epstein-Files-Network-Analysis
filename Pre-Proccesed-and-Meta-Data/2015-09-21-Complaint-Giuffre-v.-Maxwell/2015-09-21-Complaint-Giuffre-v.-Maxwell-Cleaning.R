02b171394c5e92c88469a8bdfeb7f56094c99b89
Sebas770 <seromeropa@unal.edu.co>
2026-02-16 16:20:28 -0500

    2015 Giuffre-v.-Maxwell

--- Pre-Proccesed-and-Meta-Data/2015-09-21-Complaint-Giuffre-v.-Maxwell/2015-09-21-Complaint-Giuffre-v.-Maxwell-Cleaning.R
+++ Pre-Proccesed-and-Meta-Data/2015-09-21-Complaint-Giuffre-v.-Maxwell/2015-09-21-Complaint-Giuffre-v.-Maxwell-Cleaning.R
@@ -1,4 +1,4 @@
-##Cleaning Data 2021-08-09-Complaint-Giuffre-v.-Prince Andrew
+##Cleaning Data 2015-09-21-Complaint-Giuffre-v.-Maxwell
 # 0. Load libraries
 library(tidyverse)
 library(readr)
@@ -6,11 +6,11 @@ library(pdftools)
 
 # 1. Read the pdf file
 
-raw_text <- pdf_text("gov.uscourts.nysd.564713.1.0_5.pdf")
+raw_text <- pdf_text("gov.uscourts.nysd.447706.1.0_8.pdf")
 
 # 2. Delete first and last page
 
-raw_text <- raw_text[2:14]
+raw_text <- raw_text[2:11]
 
 # 3. Joint all the text
 
@@ -18,12 +18,12 @@ raw_text <- paste(raw_text, collapse = "\n")
 
 # 4. Save as a file txt
 
-write_file(raw_text, "2021-08-09-Complaint-Giuffre-v.-Prince Andrew.txt")
+write_file(raw_text, "2015-09-21-Complaint-Giuffre-v.-Maxwell.txt")
 
 
 # 5. Load the txt file.
 
-raw_text <- read_file("2021-08-09-Complaint-Giuffre-v.-Prince Andrew.txt")
+raw_text <- read_file("2015-09-21-Complaint-Giuffre-v.-Maxwell.txt")
 
 
 
@@ -31,7 +31,7 @@ raw_text <- read_file("2021-08-09-Complaint-Giuffre-v.-Prince Andrew.txt")
 text_clean <- raw_text %>%
   
   # Remove page headers (e.g., "Case 1:21-cv-06702-LAK Document 1 Filed 08/09/21 Page 2 of 15")
-  str_remove_all("Case 1:21-cv-06702-LAK Document 1 Filed 08/09/21 Page [0-9]+ of 15") %>% 
+  str_remove_all("Case 1:15-cv-07433-LAP Document 1 Filed 09/21/15 Page [0-9]+ of 12") %>% 
   
   # Replace line breaks (Enters) with a space
   str_replace_all("\n", " ") %>%
@@ -42,4 +42,4 @@ text_clean <- raw_text %>%
 
 
 # 7. Save as a .txt
-write_file(text_clean, "2021-08-09-Complaint-Giuffre-v.-Prince Andrew-C.txt")
+write_file(text_clean, "2015-09-21-Complaint-Giuffre-v.-Maxwell-C.txt")
